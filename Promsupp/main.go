1c7a69a892dc364a2e33c1a9d9cf4c982afc51bd
Bin Lun Li <mike911209@gmail.com>
2025-02-01 01:47:20 +0800

    feat(Promsupp): fix prometheus endpoint svc, one ksvc with one promsupp svc

--- Promsupp/main.go
+++ Promsupp/main.go
@@ -24,15 +24,15 @@ var ignoreList = []string{
 
 type PromSupport struct{}
 
-func (PS *PromSupport) createService(clientset *kubernetes.Clientset, namespace string, revisionName string, ownerReferences []metav1.OwnerReference) error {
-	log.Printf("Creating Kubernetes service: %s-promservice", revisionName)
+func (PS *PromSupport) operateService(clientset *kubernetes.Clientset, namespace string, svcName string, revisionName string, ownerReferences []metav1.OwnerReference, create bool) error {
+	log.Printf("Creating Kubernetes service: %s-promservice", svcName)
 	service := &v1.Service{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:            revisionName + "-promservice",
+			Name:            svcName + "-promservice",
 			Namespace:       namespace,
 			OwnerReferences: ownerReferences,
 			Labels: map[string]string{
-				"app": revisionName,
+				"app": svcName,
 			},
 		},
 		Spec: v1.ServiceSpec{
@@ -49,28 +49,37 @@ func (PS *PromSupport) createService(clientset *kubernetes.Clientset, namespace 
 		},
 	}
 
-	_, err := clientset.CoreV1().Services(namespace).Create(context.TODO(), service, metav1.CreateOptions{})
-	if err != nil {
-		return fmt.Errorf("error creating service: %v", err)
+	if create {
+		_, err := clientset.CoreV1().Services(namespace).Create(context.TODO(), service, metav1.CreateOptions{})
+		if err != nil {
+			return fmt.Errorf("error creating service: %v", err)
+		}
+		log.Printf("Kubernetes Service for '%s' created successfully.\n", svcName)
+	} else {
+		_, err := clientset.CoreV1().Services(namespace).Update(context.TODO(), service, metav1.UpdateOptions{})
+		if err != nil {
+			return fmt.Errorf("error updating service: %v", err)
+		}
+		log.Printf("Kubernetes Service for '%s' updated successfully.\n", svcName)
 	}
-	log.Printf("Kubernetes Service for '%s' created successfully.\n", revisionName)
+
 	return nil
 }
 
-func (PS *PromSupport) createServiceMonitor(clientset *kubernetes.Clientset, namespace string, revisionName string, ownerReferences []metav1.OwnerReference) error {
-	log.Printf("Creating Prometheus ServiceMonitor: %s-servicemonitor", revisionName)
+func (PS *PromSupport) createServiceMonitor(monitoringClient *monitoringclient.MonitoringV1Client, namespace string, svcName string, ownerReferences []metav1.OwnerReference) error {
+	log.Printf("Creating Prometheus ServiceMonitor: %s-servicemonitor", svcName)
 
 	// Define the ServiceMonitor object
 	serviceMonitor := &monitoringv1.ServiceMonitor{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:            revisionName + "-servicemonitor",
+			Name:            svcName + "-servicemonitor",
 			Namespace:       namespace,
 			OwnerReferences: ownerReferences, // Set OwnerReferences if applicable
 		},
 		Spec: monitoringv1.ServiceMonitorSpec{
 			Selector: metav1.LabelSelector{
 				MatchLabels: map[string]string{
-					"app": revisionName,
+					"app": svcName,
 				},
 			},
 			Endpoints: []monitoringv1.Endpoint{
@@ -86,18 +95,12 @@ func (PS *PromSupport) createServiceMonitor(clientset *kubernetes.Clientset, nam
 		},
 	}
 
-	config, _ := rest.InClusterConfig()
-	monitoringClient, err := monitoringclient.NewForConfig(config)
-	if err != nil {
-		return fmt.Errorf("error creating monitoring client: %v", err)
-	}
-
-	_, err = monitoringClient.ServiceMonitors(namespace).Create(context.Background(), serviceMonitor, metav1.CreateOptions{})
+	_, err := monitoringClient.ServiceMonitors(namespace).Create(context.Background(), serviceMonitor, metav1.CreateOptions{})
 
 	if err != nil {
 		return fmt.Errorf("error creating service monitor: %v", err)
 	}
-	log.Printf("Prometheus ServiceMonitor for '%s' created successfully.\n", revisionName)
+	log.Printf("Prometheus ServiceMonitor for '%s' created successfully.\n", svcName)
 	return nil
 }
 
@@ -114,64 +117,70 @@ func (PS *PromSupport) Run() {
 	clientset, _ := kubernetes.NewForConfig(config)
 
 	for { // check within interval time
-
-		revisions, err := client.ListRevisions(context.Background())
+		services, err := client.ListServices(context.Background())
 		if err != nil {
 			log.Fatalf("Failed to list Knative services in namespace %s: %v", "default", err)
 		}
-		for _, revision := range revisions.Items { // iterate through all Knative services in namespace default
-			// Check if the revision name contains any of the ignore list items
+		for _, svc := range services.Items { // iterate through all Knative services in namespace default
+			// Check if the service name contains any of the ignore list items
 			ignore := false
 			for _, ignoreItem := range ignoreList {
-				if strings.Contains(revision.Name, ignoreItem) {
+				if strings.Contains(svc.Name, ignoreItem) {
 					ignore = true
 					break
 				}
 			}
 			if ignore {
-				log.Printf("Ignoring revision: %s", revision.Name)
+				log.Printf("Ignoring ksvc: %s", svc.Name)
 				continue
 			}
 
-			log.Println("Checking knative revision prom support : ", revision.Name)
-			revisionName := revision.Name
-			revisionUID := revision.UID
+			log.Println("Checking knative service prom support: ", svc.Name)
+			svcName := svc.Name
+			svcUID := svc.UID
 			ownerReferences := []metav1.OwnerReference{
 				{
 					APIVersion: "serving.knative.dev/v1",
-					Kind:       "Revision",
-					Name:       revisionName,
-					UID:        revisionUID,
+					Kind:       "Service",
+					Name:       svcName,
+					UID:        svcUID,
 				},
 			}
 
 			// Check if the service already exists
-			_, err := clientset.CoreV1().Services("default").Get(context.Background(), revisionName+"-promservice", metav1.GetOptions{})
+			k8ssvc, err := clientset.CoreV1().Services("default").Get(context.Background(), svcName+"-promservice", metav1.GetOptions{})
 			if err != nil {
 				if errors.IsNotFound(err) {
 					// Service does not exist, create it
-					log.Printf("promservice not found , Creating promservice: %s-promservice", revisionName)
-					err = PS.createService(clientset, "default", revisionName, ownerReferences)
+					log.Printf("promservice not found , Creating promservice: %s-promservice", svcName)
+					err = PS.operateService(clientset, "default", svcName, svc.Status.LatestReadyRevisionName, ownerReferences, true)
 					if err != nil {
 						log.Printf("Error creating service: %v", err)
 					}
 				} else {
 					log.Printf("Error checking for service: %v", err)
 				}
+			} else if k8ssvc.Spec.Selector["app"] != svc.Status.LatestReadyRevisionName {
+				// Service exists but selector is not updated
+				log.Printf("Service %s has new revision, updating the service", svcName+"-promservice")
+				err = PS.operateService(clientset, "default", svcName, svc.Status.LatestReadyRevisionName, ownerReferences, false)
+				if err != nil {
+					log.Printf("Error updating service: %v", err)
+				}
 			} else {
-				log.Printf("Service %s already exists", revisionName+"-promservice")
+				log.Printf("Service %s already exists", svcName+"-promservice")
 			}
 
 			config, _ := rest.InClusterConfig()
-			monitoringClient, err := monitoringclient.NewForConfig(config)
+			monitoringClient, _ := monitoringclient.NewForConfig(config)
 
 			// Check if the ServiceMonitor already exists
-			_, err = monitoringClient.ServiceMonitors("default").Get(context.Background(), revisionName+"-servicemonitor", metav1.GetOptions{})
+			_, err = monitoringClient.ServiceMonitors("default").Get(context.Background(), svcName+"-servicemonitor", metav1.GetOptions{})
 			if err != nil {
 				if errors.IsNotFound(err) {
 					// ServiceMonitor does not exist, create it
-					log.Println("ServiceMonitor not found , Creating ServiceMonitor: ", revisionName+"-servicemonitor")
-					err = PS.createServiceMonitor(clientset, "default", revisionName, ownerReferences)
+					log.Println("ServiceMonitor not found , Creating ServiceMonitor: ", svcName+"-servicemonitor")
+					err = PS.createServiceMonitor(monitoringClient, "default", svcName, ownerReferences)
 					if err != nil {
 						log.Printf("Error creating ServiceMonitor: %v", err)
 					}
@@ -179,10 +188,10 @@ func (PS *PromSupport) Run() {
 					log.Printf("Error checking for ServiceMonitor: %v", err)
 				}
 			} else {
-				log.Printf("ServiceMonitor %s already exists", revisionName+"-servicemonitor")
+				log.Printf("ServiceMonitor %s already exists", svcName+"-servicemonitor")
 			}
 		}
-		time.Sleep(1)
+		time.Sleep(100 * time.Millisecond)
 	}
 }
 
